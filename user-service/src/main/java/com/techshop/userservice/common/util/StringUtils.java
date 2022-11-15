c2db03f4dac07b388fb58c9e831861c8b4f54e56
Phạm Tấn khang <68799995+thsnd-kg@users.noreply.github.com>
2022-11-15 20:55:02 +0700

    feat: config basic user module (#6)

---
+++ user-service/src/main/java/com/techshop/userservice/common/util/StringUtils.java
@@ -0,0 +1,12 @@
+package com.techshop.userservice.common.util;
+
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
+public class StringUtils {
+    public static String deAccent(String str) {
+        String nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(nfdNormalizedString).replaceAll("").toLowerCase().trim().replaceAll(" ","-");
+    }
+}
---
+++ user-service/src/main/java/com/techshop/userservice/entity/User.java
@@ -0,0 +1,56 @@
+package com.techshop.userservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.techshop.userservice.common.entity.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "techshop_user")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false)
+    private Long userId;
+
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @JsonIgnore
+    @NotNull
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+    private String address;
+    private String imgUrl;
+    private String dateOfBirth;
+
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @Column(columnDefinition = "varchar(1) default 'Y'")
+    private String activeFlag = "Y";
+
+//    @ManyToOne
+//    @JoinColumn(name = "role_id")
+//    private Role role;
+
+
+
+}
